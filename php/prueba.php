907aabb3ebf4a2bdc57fe1ae7d432caaa2c0dd7e
FreddyVilla94 <fevig1994@gmail.com>
2016-11-21 12:25:38 -0600

    fghj

--- php/prueba.php
+++ php/prueba.php
@@ -11,9 +11,19 @@
                     $id = $row['id'];
                     $resultadoI = mysqli_query($conexion,"SELECT id FROM servicios  WHERE instalacion_id = '$id'");
                     while($rowI = mysqli_fetch_array($resultadoI)){
-                        $idSer = $rowI[id];
+                        $idSer = $rowI['id'];
                         mysqli_query($conexion, "DELETE FROM servicios where id = '$idSer'");
                     } 
+                    $resultadoII = mysqli_query($conexion,"SELECT id FROM evaluacion  WHERE instalacion_id = '$id'");
+                    while($rowII = mysqli_fetch_array($resultadoII)){
+                        $idEva = $rowII['id'];
+                        $resultadoIII = mysqli_query($conexion,"SELECT id FROM respuesta  WHERE evaluacion_id = '$idEva'");
+                        while($rowIII = mysqli_fetch_array($resultadoIII)){
+                            $idRes = $rowIII['id'];
+                            mysqli_query($conexion, "DELETE FROM respuesta where id = '$idRes'");
+                        }
+                        mysqli_query($conexion, "DELETE FROM evaluacion where id = '$idEva'");
+                    }
                     mysqli_query($conexion, "DELETE FROM instalacion WHERE id = '$id'");
                 }
                 include("cerrar_conexion.php");
